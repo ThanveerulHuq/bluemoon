0bb333c076c26cd4977dcc67a3478ead7728b4be
thanveer <thanveersiddiq@gmail.com>
2018-03-16 19:03:13 +0530

    StudentInfo Repo created

---
+++ src/main/java/com/sdms/model/StudentInfoModel.java
@@ -0,0 +1,198 @@
+package com.sdms.model;
+
+import java.util.Date;
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
+
+public class StudentInfoModel {
+	private Long studentId;
+
+	private Long aadharNo;
+
+	private Long admissionNo;
+
+	private Long emisNo;
+
+	private String name;
+
+	private String gender;
+
+	private Long age;
+
+	private Date dob;
+
+	private String community;
+
+	private String motherTongue;
+
+	private Long mobileNo1;
+
+	private Long mobileNo2;
+
+	private String fatherName;
+
+	private String motherName;
+
+	private String address;
+
+	private String previousSchool;
+
+	private String active;
+
+	private Date admissionDate;
+
+	private List<MultipartFile> images;
+
+	public Long getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Long studentId) {
+		this.studentId = studentId;
+	}
+
+	public Long getAadharNo() {
+		return aadharNo;
+	}
+
+	public void setAadharNo(Long aadharNo) {
+		this.aadharNo = aadharNo;
+	}
+
+	public Long getAdmissionNo() {
+		return admissionNo;
+	}
+
+	public void setAdmissionNo(Long admissionNo) {
+		this.admissionNo = admissionNo;
+	}
+
+	public Long getEmisNo() {
+		return emisNo;
+	}
+
+	public void setEmisNo(Long emisNo) {
+		this.emisNo = emisNo;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Long getAge() {
+		return age;
+	}
+
+	public void setAge(Long age) {
+		this.age = age;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public String getCommunity() {
+		return community;
+	}
+
+	public void setCommunity(String community) {
+		this.community = community;
+	}
+
+	public String getMotherTongue() {
+		return motherTongue;
+	}
+
+	public void setMotherTongue(String motherTongue) {
+		this.motherTongue = motherTongue;
+	}
+
+	public Long getMobileNo1() {
+		return mobileNo1;
+	}
+
+	public void setMobileNo1(Long mobileNo1) {
+		this.mobileNo1 = mobileNo1;
+	}
+
+	public Long getMobileNo2() {
+		return mobileNo2;
+	}
+
+	public void setMobileNo2(Long mobileNo2) {
+		this.mobileNo2 = mobileNo2;
+	}
+
+	public String getFatherName() {
+		return fatherName;
+	}
+
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+
+	public String getMotherName() {
+		return motherName;
+	}
+
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPreviousSchool() {
+		return previousSchool;
+	}
+
+	public void setPreviousSchool(String previousSchool) {
+		this.previousSchool = previousSchool;
+	}
+
+	public String getActive() {
+		return active;
+	}
+
+	public void setActive(String active) {
+		this.active = active;
+	}
+
+	public Date getAdmissionDate() {
+		return admissionDate;
+	}
+
+	public void setAdmissionDate(Date admissionDate) {
+		this.admissionDate = admissionDate;
+	}
+
+	public List<MultipartFile> getImages() {
+		return images;
+	}
+
+	public void setImages(List<MultipartFile> images) {
+		this.images = images;
+	}
+
+}
---
+++ src/main/java/com/sdms/repository/StudentsInfoRepo.java
@@ -0,0 +1,9 @@
+package com.sdms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sdms.entity.StudentsInfo;
+
+public interface StudentsInfoRepo extends JpaRepository<StudentsInfo,Long>{
+
+}
